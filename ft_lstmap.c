9357bc81391f54a19a5624c3c734562c2f1dcda0
Desulme Briant <dbriant@cbr13s3.42barcelona.com>
2025-01-27 20:26:34 +0100

    added

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: dbriant <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 13:10:46 by dbriant           #+#    #+#             */
-/*   Updated: 2025/01/27 13:39:59 by dbriant          ###   ########.fr       */
+/*   Updated: 2025/01/27 20:26:04 by dbriant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -17,14 +17,17 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*temp;
 	t_list	*newlist;
 
-	newlist = ft_lstnew(lst->content);
-	ft_lstiter(newlist, f);
-	while (temp)
+	newlist = NULL;
+	while (lst)
 	{
-		temp = lst->next;
-		ft_lstiter(lst, f);
-		newlist->content = lst->content;
-		newlist->next = lst->next;
-		ft_lstclear(*lst, del)
+		temp = ft_lstnew(f(lst->content));
+		if (!temp)
+		{
+			ft_lstclear(&temp, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&newlist, temp);
+		lst = lst->next;
 	}
+	return (newlist);
 }
